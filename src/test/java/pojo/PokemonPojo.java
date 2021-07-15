5524790c8049104dbfb7832c61b1e49c323d6c94
JyldyzJakypova <74631873+JyldyzJakypova@users.noreply.github.com>
2021-07-15 19:42:48 -0400

    training on soft skills

---
+++ src/test/java/pojo/PokemonPojo.java
@@ -0,0 +1,20 @@
+package pojo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.Map;
+
+@Getter
+@Setter
+public class PokemonPojo {
+
+    private int count;
+    private String next;
+    private String previous;
+    private List<Map<String, String>> results;
+
+
+    }
+
